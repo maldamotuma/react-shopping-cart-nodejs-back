31813dca8eac9b67912b4ba3714006a81462ab12
malda <maldamotuma@gmail.com>
2021-02-07 09:34:27 +0300

    create product component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 // feature one (1) tokko
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">react shopping cart</a>
-      </header>
-      <main>
-        product list
-      </main>
-      <footer>
-        all right is reserved.
-      </footer>
-    </div>
-  );
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">react shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">
+              cart items
+            </div>
+          </div>
+        </main>
+        <footer>
+          all right is reserved.
+        </footer>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
