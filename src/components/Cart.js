feefb4cb3c9521935026128e58f58aaef7aa3fbd
malda <maldamotuma@gmail.com>
2021-02-07 13:39:52 +0300

    cart component

---
+++ src/components/Cart.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import formatCurrency from '../utils';
+
+export default class Cart extends Component {
+    render() {
+        const {cartItems} = this.props;
+        return (
+            <div>
+                {cartItems.length === 0 ? <div className="cart cart-header">Cart is Empty</div>
+                : <div className="cart cart-header">you have { cartItems.length } in the cart {" "}</div> }
+            <div>
+                <div className="cart">
+                    <ul className="cart-items">
+                    
+                        {cartItems.map(item => (
+                            <li key={item.id}>
+                                <div>
+                                    <img key={item.id} src={item.image} alt={item.title} />
+                                </div>
+                                <div>
+                                    <div>
+                                        {item.title}
+                                    </div>
+                                    <div className="right">
+                                        {formatCurrency(item.price)} * {item.count} {" "}
+                                        <button
+                                        className="button"
+                                        onClick={ e=> this.props.removeFromCart(item)}
+                                        >
+                                            Remove
+                                        </button>
+                                    </div>
+                                    
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                {cartItems.length !==0 && (
+                    <div className="cart">
+                        <div className="total">
+                            <div>
+                            Total : {" "}
+                                {formatCurrency(cartItems.reduce((a,c)=>a = a + (c.price * c.count),0))}
+                            </div>
+                            <button className="button primary">
+                                proceed
+                            </button>
+                        </div>
+                    </div>
+                )}
+                
+            </div>
+            </div>
+        )
+    }
+}
