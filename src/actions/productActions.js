32a8696155c96afde8db3e183fd692d92e544d6b
malda <maldamotuma@gmail.com>
2021-02-08 22:02:08 +0300

    add redux to filter component

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { FETCH_PRODUCT } from "../types";
+import { FETCH_PRODUCT, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
 
 export const fetchProducts = () => async (dispatch) => {
     const res = await fetch("/api/products");
@@ -6,5 +6,34 @@ export const fetchProducts = () => async (dispatch) => {
     dispatch({
         type: FETCH_PRODUCT,
         payload: data
+    });
+}
+
+export const filterProducts = (products, size) => dispatch => {
+    dispatch({
+        type: FILTER_PRODUCTS_BY_SIZE,
+        payload: {
+            size,
+            items: size === "" ? products :
+            products.filter(product => (product.availableSizes.indexOf(size)>=0))
+        }
+    });
+}
+
+export const sortProducts = (filteredProducts, sort) => dispatch => {
+    const sortedProducts = filteredProducts.slice();
+    if(sort === "") {
+        sortedProducts.sort((a,b)=>(a._id > b._id ? 1 : -1));
+    }else {
+        sortedProducts.sort((a,b) => (
+        sort === "lowest" ? a.price > b.price ? 1 : -1 :
+        a.price > b.price ? -1 : 1 ))
+    }
+    dispatch({
+        type: ORDER_PRODUCTS_BY_PRICE,
+        payload: {
+            sort,
+            items: sortedProducts
+        }
     });
 }
--- src/reducer/productReducers.js
+++ src/reducer/productReducers.js
@@ -1,10 +1,21 @@
-import { FETCH_PRODUCT } from "../types";
+import { FETCH_PRODUCT, FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE } from "../types";
 
 export const productReducer = (state={}, action) => {
     switch (action.type) {
+        case ORDER_PRODUCTS_BY_PRICE:
+            return {
+                ...state,
+                sort: action.payload.sort,
+                filteredItems: action.payload.items
+            }
+        case FILTER_PRODUCTS_BY_SIZE:
+            return {
+                ...state,
+                size: action.payload.size,
+                filteredItems: action.payload.items
+            }
         case FETCH_PRODUCT:
-            console.log('the payload is : '+action.payload);
-            return {items: action.payload}
+            return {items: action.payload, filteredItems: action.payload}
     
         default:
             return state;
